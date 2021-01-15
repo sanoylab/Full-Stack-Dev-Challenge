ca4f9f0d0cf39909704218d3723dea8d54a9f82d
Yonas <expertsanoy@gmail.com>
2021-01-14 19:52:55 -0500

    adding asp.net mvc 5 boilerplate

---
+++ asp_net_mvc5/Online.Classified.App/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online.Classified.App.Controllers
+{
+    public class HomeController: Controller
+    {
+        public string Index()
+        {
+            return "ASP.NET MVC 5 APP";
+        }
+    }
+}
